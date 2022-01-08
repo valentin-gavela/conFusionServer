3b7f9dd5170507d7ea32625fa9062ee2a55dfa14
valentin-gavela <valentingavela@gmail.com>
2022-01-08 15:26:19 -0300

    Basic Authentication

--- app.js
+++ app.js
@@ -19,9 +19,37 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  const auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const [user, pass] = auth;
+
+  if (user == 'admin' && pass == 'password') {
+    next(); // authorized
+  } else {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
+
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,12 +61,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -51,8 +79,8 @@ app.use(function(err, req, res, next) {
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
-connect.then((db) => {
-    console.log("Connected correctly to server");
+connect.then((_db) => {
+  console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
 
 module.exports = app;
