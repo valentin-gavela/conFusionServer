98cb5785704c61490194005f778d90c041c1907f
valentin-gavela <valentingavela@gmail.com>
2022-01-22 21:11:36 -0300

    HTTPS

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 // const session = require('express-session');
 // const FileStore = require('session-file-store')(session);
-const passport = require('passport');
-const config = require('./config');
+const passport = require("passport");
+const config = require("./config");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-const dishRouter = require('./routes/dishRouter');
-const promoRouter = require('./routes/promotionRouter');
-const leaderRouter = require('./routes/leaderRouter');
+const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promotionRouter");
+const leaderRouter = require("./routes/leaderRouter");
 
 const app = express();
 
+// Secure traffic only
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(
+      307,
+      "https://" + req.hostname + ":" + app.get("secPort") + req.url
+    );
+  }
+});
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // app.use(cookieParser('12345-67890-09876-54321'));
 
@@ -34,20 +46,19 @@ app.set('view engine', 'jade');
 
 app.use(passport.initialize());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -58,18 +69,23 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
-connect.then((_db) => {
-  console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+connect.then(
+  (_db) => {
+    console.log("Connected correctly to server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
 module.exports = app;
