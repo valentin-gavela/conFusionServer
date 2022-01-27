c6f8a4ea70760855d5532e7ed124e8d0a936ea79
valentin-gavela <valentingavela@gmail.com>
2022-01-27 10:31:13 -0300

    facebook authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
-}
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "955828651703051",
+    clientSecret: "06b9cae75f04b9438736f0d4a87547fd",
+  },
+};
